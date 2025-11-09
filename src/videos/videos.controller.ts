742fe96c0859c9248fc6b2b137819ac455785c4a
rob cowley <robcowley06@hotmail.co.uk>
2025-11-09 15:23:23 +0000

    WIP

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -9,13 +9,17 @@ export class VideoController {
     constructor(private readonly videosService: VideosService) {}
 
     @Post()
-    @UseGuards(CognitoAuthGuard)
+    // @UseGuards(CognitoAuthGuard)
     async handleVideoAction(@Body() dto: { body: VideoActionDto }, @Req() req): Promise<VideoResponse> {
-        return this.videosService.handleVideoAction(dto.body, req.user['cognito:username'], req.user['name']);
+        console.log('req.user', req.user);
+        console.log('req.body', dto);
+        const userId = req.user? req.user['cognito:username'] : null;
+        const name = req.user? req.user['name'] : null;
+        return this.videosService.handleVideoAction(dto.body, userId, name);
     }
 
     @Get()
-    @UseGuards(CognitoAuthGuard)
+    // @UseGuards(CognitoAuthGuard)
     async getAllVideos(): Promise<any> {
         return this.videosService.getAllVideos();
     }
@@ -27,7 +31,7 @@ export class VideoController {
     }
 
     @Get(':id')
-    @UseGuards(CognitoAuthGuard)
+    // @UseGuards(CognitoAuthGuard)
     async getVideoMetadata(@Param('id') id: string): Promise<any> {
         return this.videosService.getVideoMetadata(id);
     }
